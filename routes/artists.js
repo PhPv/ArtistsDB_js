fc39046491228df5d9c39872905172f957ca7e54
Slava <slavok15@yandex.ru>
2020-04-29 12:07:07 +0400

    routes upd

--- routes/artists.js
+++ routes/artists.js
@@ -2,9 +2,9 @@ const express = require('express')
 const controller = require('../controllers/artists')
 const router = express.Router()
 
-router.post('/', controller.create)
-router.get('/', controller.getArtist)
-router.put('/', controller.putArtistByName)
-router.delete('/', controller.deleteArtistByName) // реализовать
+router.post('/create.artist', controller.create)
+router.get('/getArtist', controller.getArtist) //сделать список из имён отсортированных
+router.put('/putArtists', controller.putArtistByName)
+router.delete('/deleteArtist', controller.deleteArtistByName)
 
 module.exports = router
