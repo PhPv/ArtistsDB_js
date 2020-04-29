ae54e8670532f38052655b4e1a063ca7e4563dbb
Slava <slavok15@yandex.ru>
2020-04-29 12:11:21 +0400

    second

--- routes/artists.js
+++ routes/artists.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/artists')
 const router = express.Router()
 
 router.post('/create.artist', controller.create)
-router.get('/getArtist', controller.getArtist) //сделать список из имён отсортированных
+router.get('/getArtist', controller.getArtist) // сделать список из имён отсортированных
 router.put('/putArtists', controller.putArtistByName)
 router.delete('/deleteArtist', controller.deleteArtistByName)
 
